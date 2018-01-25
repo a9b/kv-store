b07bf49aa09a67d49a0af0929a89e9166a407673
a9b <a9b.word@gmail.com>
2018-01-25 15:19:38 +0900

    via. https://qiita.com/_P0cChi_/items/ebf8fbf035b36218a37e

---
+++ src/components/modules/HeadComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ title }}</div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'headComp',
+  computed: mapGetters({
+    // ゲッターを異なる名前でマッピング (via: https://vuex.vuejs.org/ja/getters.html
+    title: 'getTitle'
+  })
+}
+</script>
