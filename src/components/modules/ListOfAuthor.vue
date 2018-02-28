c59efc1846a781c40fed79963f729e33de9d70cd
a9b <a9b.word@gmail.com>
2018-02-28 14:16:46 +0900

    Add validation

--- src/components/modules/ListOfAuthor.vue
+++ src/components/modules/ListOfAuthor.vue
@@ -2,34 +2,34 @@
   <div>
     <div class="field columns">
       <div class="column is-12">
-        <a class="button is-link" v-if="!isAdd" v-on:click="isAdd = !isAdd">
+        <button class="button is-link" v-if="!isAdd" @click="isAdd = !isAdd">
           <span class="is-right">
             <icon name="plus"></icon>
             add
           </span>
-        </a>
+        </button>
       </div>
     </div>
     <div v-if="isAdd" class="field columns">
       <div class="column control is-5">
-        <input class="input is-link" type="text" placeholder="key" v-model="addItem.key">
+        <input class="input is-link" type="text" placeholder="key" v-model="additionItem.key">
       </div>
       <div class="column control is-5">
-        <input class="input is-link" type="text" placeholder="value" v-model="addItem.value">
+        <input class="input is-link" type="text" placeholder="value" v-model="additionItem.value">
       </div>
       <div class="column">
-        <a class="button is-primary is-link" v-on:click="edit(addItem.key, addItem.value)">
+        <button class="button is-primary is-link" @click="edit(additionItem.key, additionItem.value)" :disabled="!formIsValid">
           <span>
             <icon name="check"></icon>
             add
           </span>
-        </a>
-        <a class="button is-primary is-link is-outlined" v-on:click="isAdd = false">
+        </button>
+        <button class="button is-primary is-link is-outlined" @click="cancel()">
           <span>
             cancel
             <icon name="times"></icon>
           </span>
-        </a>
+        </button>
       </div>
     </div>
 
@@ -41,18 +41,18 @@
         <input class="input" type="text" v-model="item.value">
       </div>
       <div class="column">
-        <a class="button is-primary" v-on:click="edit(item.key, item.value)">
+        <button class="button is-primary" @click="edit(item.key, item.value)">
           <span>
             <icon name="check"></icon>
             edit
           </span>
-        </a>
-        <a class="button is-danger" v-on:click="del(item.key)">
+        </button>
+        <button class="button is-danger" @click="del(item.key)">
           <span>
             delete
             <icon name="times"></icon>
           </span>
-        </a>
+        </button>
       </div>
     </div>
   </div>
@@ -66,12 +66,23 @@ export default {
   data () {
     return {
       isAdd: false,
-      addItem: {},
+      additionItem: {},
       flg: false,
       targetName: this.$route.params.userId
     }
   },
   methods: {
+    cancel: function () {
+      this.isAdd = false
+      this.additionItem = {}
+    },
+    del: function (key) {
+      this.$store.dispatch('deleteKV', {'key': key}).then(() => {
+        this.$store.dispatch('fetchKVs', this.targetName)
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
     edit: function (key, value) {
       this.$store.dispatch('insertKV', {'key': key, 'value': value}).then(() => {
         this.$store.dispatch('fetchKVs', this.targetName)
@@ -80,16 +91,18 @@ export default {
       }).catch((err) => {
         console.log(err)
       })
-    },
-    del: function (key) {
-      this.$store.dispatch('deleteKV', {'key': key}).then(() => {
-        this.$store.dispatch('fetchKVs', this.targetName)
-      }).catch((err) => {
-        console.log(err)
-      })
     }
   },
   computed: {
+    formIsValid () {
+      return this.additionItem.key !== undefined &&
+             this.additionItem.value !== undefined &&
+             String(this.additionItem.key).length > 0 &&
+             String(this.additionItem.value).length > 0 &&
+             // http://www-creators.com/archives/1827
+             // 記号は許可しない
+             !/^!"#$%&'()\*\+-\.,\/:;<=>?@\[\\\]^_`{|}~]+/.test(this.additionItem.key)
+    },
     ...mapGetters([
       'items',
       'itemsCount'
@@ -97,6 +110,3 @@ export default {
   }
 }
 </script>
-
-<style>
-</style>
